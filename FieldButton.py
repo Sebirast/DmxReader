2e70d7a0e02bfc183f905a386d46631b32aba3ac
Sebastian Rast <sebastian.rast@outlook.com>
2022-11-27 19:52:23 +0000

    deleted unnecessary files

--- FieldButton.py
+++
@@ -1,7 +0,0 @@
-from tkinter import *
-
-class FieldButton(Button):
-    idx = 0
-    def __init__(self, master, idx, text="", fg="black", width=50, height=10):
-        Button.__init__(self, master, text=text, fg=fg, width=width, height=height)
-        self.idx = idx
--- Widget.py
+++
@@ -1,5 +0,0 @@
-from tkinter import *
-
-class MyWidget(Frame):
-    def __init__(self, master, background="#000000", width=100):
-        Frame.__init__(self, master, background=background, width=width)
